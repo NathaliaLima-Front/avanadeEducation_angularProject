ddc271e66da811dc450585a9728f1543288213ae
Nathália Lima <nathalia.lima@avanade.com>
2022-08-18 10:11:47 -0300

    add routing pag principal + variables + fonts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailLessonComponent } from './views/detail-lesson/detail-lesson.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DetailLessonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
